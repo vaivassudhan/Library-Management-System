4cdb6269cae5c1cb89428570ad8a52e9c8d00b11
vaivassudhan <vaivassaravanan1@gmail.com>
2022-03-30 17:40:05 +0530

    update groups

--- src/main/java/Group_days/AddGroupServlet.java
+++ src/main/java/Group_days/AddGroupServlet.java
@@ -2,10 +2,14 @@ package Group_days;
 
 import Fine.Fine;
 import Fine.FineDao;
+import Utils.Util;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddGroupServlet extends HttpServlet {
     @Override
@@ -15,28 +19,42 @@ public class AddGroupServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getSession().getAttribute("Librarian_Id") == null){
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.html");
-            dispatcher.forward(request,response);
-        }
+//        if(request.getSession().getAttribute("Librarian_Id") == null){
+//            RequestDispatcher dispatcher = request.getRequestDispatcher("login.html");
+//            dispatcher.forward(request,response);
+//        }
         GroupDays groupDays = new GroupDays();
-        groupDays.setDays(Integer.parseInt(request.getParameter("Days")));
+//      Handling json request
+        String jb = Util.jsonRequestHandler(request);
+        System.out.println("HELLO " + jb);
+
+        JsonObject jsonObject = new JsonParser().parse(jb).getAsJsonObject();
+
+//      Get Values from json
+        int days = jsonObject.get("Days").getAsInt();
+        float fine_per_day = jsonObject.get("Fine_Per_Day").getAsInt();
+
+        groupDays.setDays(days);
+
         int group_id = GroupDaysDAO.addGroup(groupDays);
         Fine fine = new Fine();
         fine.setGroup_Id(group_id);
-        fine.setFine_Per_Day(Integer.parseInt(request.getParameter("Fine_Per_Day")));
+        fine.setFine_Per_Day(fine_per_day);
         int status = FineDao.addFine(fine);
+
+        JsonObject jsonobject = new JsonObject();
+        PrintWriter out = response.getWriter();
         if(status != 0 ){
-            request.setAttribute("message-type","success");
-            request.setAttribute("message","Group Added Successfully");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("message.jsp");
-            dispatcher.forward(request,response);
+            jsonobject.addProperty("message-type","success");
+            jsonobject.addProperty("message","Group Added Successfully");
+            out.write(String.valueOf(jsonobject));
+            out.flush();
         }
         else{
-            request.setAttribute("message-type","error");
-            request.setAttribute("message","Error Occurred");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("message.jsp");
-            dispatcher.forward(request,response);
+            jsonobject.addProperty("message-type","error");
+            jsonobject.addProperty("message","Some Error Occurred");
+            out.write(String.valueOf(jsonobject));
+            out.flush();
         }
     }
 }
--- src/main/java/Group_days/UpdateDaysServlet.java
+++ src/main/java/Group_days/UpdateDaysServlet.java
@@ -1,8 +1,13 @@
 package Group_days;
 
+import Utils.Util;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class UpdateDaysServlet extends HttpServlet {
     @Override
@@ -12,21 +17,31 @@ public class UpdateDaysServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            int group_id = Integer.parseInt(request.getParameter("Group_Id"));
-            int days = Integer.parseInt(request.getParameter("Days"));
+        GroupDays groupDays = new GroupDays();
+//      Handling json request
+        String jb = Util.jsonRequestHandler(request);
+        System.out.println("HELLO " + jb);
 
-            int status = GroupDaysDAO.updateGroup(group_id,days);
-            if(status != 0 ){
-                request.setAttribute("message-type","success");
-                request.setAttribute("message","Group Updated Successfully");
-                RequestDispatcher dispatcher = request.getRequestDispatcher("message.jsp");
-                dispatcher.forward(request,response);
-            }
-            else{
-                request.setAttribute("message-type","error");
-                request.setAttribute("message","Error Occurred");
-                RequestDispatcher dispatcher = request.getRequestDispatcher("message.jsp");
-                dispatcher.forward(request,response);
-            }
+        JsonObject jsonObject = new JsonParser().parse(jb).getAsJsonObject();
+
+        int group_id = jsonObject.get("Group_Id").getAsInt();
+        int days = jsonObject.get("Days").getAsInt();
+
+
+        int status = GroupDaysDAO.updateGroup(group_id,days);
+        JsonObject jsonobject = new JsonObject();
+        PrintWriter out = response.getWriter();
+        if(status != 0 ){
+            jsonobject.addProperty("message-type","success");
+            jsonobject.addProperty("message","Group Updated Successfully");
+            out.write(String.valueOf(jsonobject));
+            out.flush();
+        }
+        else{
+            jsonobject.addProperty("message-type","error");
+            jsonobject.addProperty("message","Error occurred");
+            out.write(String.valueOf(jsonobject));
+            out.flush();
+        }
     }
 }
